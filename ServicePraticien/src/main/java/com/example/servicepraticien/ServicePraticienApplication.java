e5ee196676a9a5f60cd6b6eb540dc3a66f459b88
ZaherHicham <hicham.zaher@etu.unice.fr>
2025-02-19 15:42:58 +0100

    eureka running

--- ServicePraticien/src/main/java/com/example/servicepraticien/ServicePraticienApplication.java
+++ ServicePraticien/src/main/java/com/example/servicepraticien/ServicePraticienApplication.java
@@ -2,8 +2,10 @@ package com.example.servicepraticien;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServicePraticienApplication {
 
     public static void main(String[] args) {
